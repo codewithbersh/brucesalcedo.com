5d3157d415a333a99573c2fc9ac94b3054d68945
codewithbersh <121822380+codewithbersh@users.noreply.github.com>
2023-12-14 10:44:43 +0300

    feat: get in touch to open send message modal

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,4 +1,10 @@
+"use client";
+
+import { useSendMessage } from "@/hooks/use-send-message";
+
 export const Header = () => {
+  const { setIsOpen } = useSendMessage();
+
   return (
     <div className="space-y-10 px-4">
       <div className="space-y-2">
@@ -16,11 +22,19 @@ export const Header = () => {
         </h2>
       </div>
       <p
-        className=" animate-fade-up opacity-0"
+        className=" animate-fade-up text-muted-foreground opacity-0"
         style={{ animationFillMode: "forwards", animationDelay: "0.45s" }}
       >
-        A passionate developer with a love for minimal and high-quality designs.
-        Open to full-time or freelance opportunities
+        A passionate developer with a love for{" "}
+        <span className="text-primary">minimal</span> and{" "}
+        <span className="text-primary">high-quality designs</span>. Open to
+        full-time or freelance opportunities.{" "}
+        <span
+          className="cursor-pointer text-primary underline-offset-2 hover:underline"
+          onClick={() => setIsOpen(true)}
+        >
+          Get in touch.
+        </span>
       </p>
     </div>
   );
